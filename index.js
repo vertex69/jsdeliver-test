c647754e755cbadbf00f9d636224933538774be4
EmmanuelPanlican <emmanuel@iboom.fm>
2022-04-04 18:12:29 +0800

    removing scroll when modal is on

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const body = document.querySelector("body");
 const modalForm = document.querySelector(".modal-form");
-const modalWrapper = document.querySelector(".modal-wrapper");
 const signUpButton = document.querySelector(".sign-up-button");
 
 signUpButton.addEventListener("click", () => {
   modalForm.style.display = "flex";
+  body.style.overflow = "hidden";
 });
 
 modalForm.addEventListener("click", () => {
   modalForm.style.display = "none";
+  body.style.overflow = "flex";
 });
